af7398be5feeb806b747118fcdaa618c92bf2df3
renzl94 <terje.rensel@gmail.com>
2024-04-18 20:52:39 +0200

    🐳

--- node-app/index.js
+++ node-app/index.js
@@ -38,7 +38,7 @@ const http = require('http');
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('docker is easy');
+  res.end('docker 🐳');
 }).listen(8080);
 
 console.log('Server running at http://localhost:8080/');
